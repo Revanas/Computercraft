e3ed0c72beb84762ef2a26a891d686632b154203
Revanas <sieber.an@gmail.com>
2024-09-11 14:35:24 +0200

    Update run.lua

--- run.lua
+++ run.lua
@@ -2,6 +2,9 @@ os.loadAPI("inv")
 while true do
   print("checking for cole")
   inv.checkFuelStatusAndRefill(6,2)
+  print("clear furnaces")
   inv.emptyFurnaces()
+  print("check stock for cole")
+  inv.produceCharcoal(256)
   sleep(10)
 end
